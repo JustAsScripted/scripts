4119c854abc26ebb5bd529c828513e911c7176a9
JustAsScripted <107677413+JustAsScripted@users.noreply.github.com>
2022-06-17 13:59:31 +0500

    Create Budokai.lua

---
+++ Budokai.lua
@@ -0,0 +1,29 @@
+local plr = game:GetService("Players").LocalPlayer.Character
+
+
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/zxciaz/VenyxUI/main/Reuploaded"))() --someone reuploaded it so I put it in place of the original back up so guy can get free credit.
+local venyx = library.new("Budokai", 5013109572)
+
+-- themes
+local themes = {
+Background = Color3.fromRGB(24, 24, 24),
+Glow = Color3.fromRGB(0, 0, 0),
+Accent = Color3.fromRGB(10, 10, 10),
+LightContrast = Color3.fromRGB(20, 20, 20),
+DarkContrast = Color3.fromRGB(14, 14, 14),  
+TextColor = Color3.fromRGB(255, 255, 255)
+}
+
+local page = venyx:addPage("Main", 5012544693)
+local section1 = page:addSection("Main")
+section1:addButton("Godmode", function()
+pcall( function()
+    game.Players.LocalPlayer.Character.Core.Cooldowns.InvincibilityFrames:Destroy()
+end)  
+end)
+section1:addButton("Reset without loosing anything", function()
+    pcall( function()
+        game.Players.LocalPlayer.Character.Head:Destroy()
+        end)
+end)
+venyx:SelectPage(venyx.pages[1], true)
