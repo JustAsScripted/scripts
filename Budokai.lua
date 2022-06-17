e3fad4cf9e060f4ad2e3329b84bd57b88d804836
JustAsScripted <107677413+JustAsScripted@users.noreply.github.com>
2022-06-17 14:39:40 +0500

    Update Budokai.lua

--- Budokai.lua
+++ Budokai.lua
@@ -1,5 +1,5 @@
 local plr = game:GetService("Players").LocalPlayer.Character
-
+local player = game.Players.LocalPlayer
 
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/zxciaz/VenyxUI/main/Reuploaded"))() --someone reuploaded it so I put it in place of the original back up so guy can get free credit.
 local venyx = library.new("Budokai", 5013109572)
@@ -24,6 +24,11 @@ end)
 section1:addButton("Reset without loosing anything", function()
     pcall( function()
         game.Players.LocalPlayer.Character.Head:Destroy()
+        end)
+end)
+section1:addButton("Instant Log", function()
+    pcall( function()
+     player:Kick("Instant Log")
         end)
 end)
 venyx:SelectPage(venyx.pages[1], true)
