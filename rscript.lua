75dc53674e6e2c89e217650f0a86001a48c27239
JustAsScripted <107677413+JustAsScripted@users.noreply.github.com>
2023-11-30 18:25:24 +0500

    Update rscript.lua

--- rscript.lua
+++ rscript.lua
@@ -2,14 +2,13 @@ game.Players.LocalPlayer.Character.HumanoidRootPart.ChildAdded:Connect(function(
     if nChild.ClassName == "BodyVelocity" then
         task.spawn(function()
             repeat
-                if game.Players.LocalPlayer.Character.HumanoidRootPart:FindFirstChild(nChild) then
-                    nChild.MaxForce = Vector3.new(10000,10000,10000)
-                    nChild.P = 3000
-                    nChild.Velocity = Vector3.new(0,7,0)
-                end
+                game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).MaxForce = Vector3.new(10000,10000,10000)
+                game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).P = 3000
+                game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).Velocity = Vector3.new(0,7,0)
                 task.wait()
-            until not game.Players.LocalPlayer.Character.HumanoidRootPart:FindFirstChild(nChild)
+            until not game.Players.LocalPlayer.Character.HumanoidRootPart:FindFirstChild(nChild.Name)
         end)
+        wait(0.1)
         print(nChild.MaxForce)
         print(nChild.P)
         print(nChild.Velocity)
