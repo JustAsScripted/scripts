82176cb313f24da589a514d4dcae7f47b84c3f7f
JustAsScripted <107677413+JustAsScripted@users.noreply.github.com>
2023-11-30 21:36:00 +0500

    Update rscript.lua

--- rscript.lua
+++ rscript.lua
@@ -1,11 +1,11 @@
 game.Players.LocalPlayer.Character.HumanoidRootPart.ChildAdded:Connect(function(nChild)
     if nChild.ClassName == "BodyVelocity" then
         repeat
-            game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).P = 2000
+            game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).P = 2200
             if game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).Velocity.Magnitude > 40 then
-                local newVelocity = (game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).Velocity + (game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).Velocity * 0.4))
+                local newVelocity = (game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).Velocity + (game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).Velocity * 0.5))
                 repeat
-                    game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).P = 2000
+                    game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).P = 2200
                     game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).Velocity = newVelocity
                     task.wait()
                 until not game.Players.LocalPlayer.Character.HumanoidRootPart:FindFirstChild(nChild.Name)
@@ -14,4 +14,3 @@ game.Players.LocalPlayer.Character.HumanoidRootPart.ChildAdded:Connect(function(
         until not game.Players.LocalPlayer.Character.HumanoidRootPart:FindFirstChild(nChild.Name)
     end
 end)
-
