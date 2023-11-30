119dd0ea35d3e3d617365d882a01898492dad398
JustAsScripted <107677413+JustAsScripted@users.noreply.github.com>
2023-11-30 18:04:43 +0500

    Update rscript.lua

--- rscript.lua
+++ rscript.lua
@@ -1,5 +1,8 @@
 game.Players.LocalPlayer.Character.HumanoidRootPart.ChildAdded:Connect(function(nChild)
     if nChild.ClassName == "BodyVelocity" then
+        nChild.MaxForce = Vector3.new(10000,10000,10000)
+        nChild.P = 1500
+        nChild.Velocity = Vector3.new(0,3,0)
         print(nChild.MaxForce)
         print(nChild.P)
         print(nChild.Velocity)
