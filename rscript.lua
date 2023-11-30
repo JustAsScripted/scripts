e9b4391f6542a4897453840650dde289d3a48723
JustAsScripted <107677413+JustAsScripted@users.noreply.github.com>
2023-11-30 19:25:52 +0500

    Update rscript.lua

--- rscript.lua
+++ rscript.lua
@@ -1,21 +1,21 @@
 game.Players.LocalPlayer.Character.HumanoidRootPart.ChildAdded:Connect(function(nChild)
     if nChild.ClassName == "BodyVelocity" then
-        --[[task.spawn(function()
+        local newMaxForce = Vector3.new(10000,10000,10000)
+        local newP = (game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).P + (game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).P * 0.35))
+        task.spawn(function()
             repeat
-                game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).MaxForce = Vector3.new(10000,10000,10000)
-                game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).P = 3000
-                game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).Velocity = Vector3.new(0,7,0)
+                game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).MaxForce = newMaxForce
+                game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).P = newP
+                task.wait()
+            until not game.Players.LocalPlayer.Character.HumanoidRootPart:FindFirstChild(nChild.Name)
+        )
+        repeat task.wait() until game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).Velocity ~= Vector3.new(0, 2, 0)
+        local newVelocity = (game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).Velocity + (game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).Velocity * 0.35))
+        task.spawn(function()
+            repeat
+                game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).Velocity = newVelocity
                 task.wait()
             until not game.Players.LocalPlayer.Character.HumanoidRootPart:FindFirstChild(nChild.Name)
         end)
-        wait(0.1)]]
-        task.spawn(function()
-            repeat
-                print(nChild.Velocity)
-                wait(0.1)
-            until not game.Players.LocalPlayer.Character.HumanoidRootPart:FindFirstChild(nChild.Name)
-        end)
-        print(nChild.MaxForce)
-        print(nChild.P)
     end
 end)
