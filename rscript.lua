84ece0f0e960204b541dc0bf613f12b7aca5250d
JustAsScripted <107677413+JustAsScripted@users.noreply.github.com>
2023-11-30 19:47:39 +0500

    Update rscript.lua

--- rscript.lua
+++ rscript.lua
@@ -1,7 +1,7 @@
 game.Players.LocalPlayer.Character.HumanoidRootPart.ChildAdded:Connect(function(nChild)
     if nChild.ClassName == "BodyVelocity" then
         local newMaxForce = Vector3.new(10000,10000,10000)
-        local newP = (game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).P + (game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).P * 0.35))
+        local newP = (game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).P + (game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).P * 0.5))
         task.spawn(function()
             repeat
                 game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).MaxForce = newMaxForce
@@ -9,8 +9,8 @@ game.Players.LocalPlayer.Character.HumanoidRootPart.ChildAdded:Connect(function(
                 task.wait()
             until not game.Players.LocalPlayer.Character.HumanoidRootPart:FindFirstChild(nChild.Name)
         end)
-        repeat task.wait() until game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).Velocity ~= Vector3.new(0, 2, 0)
-        local newVelocity = (game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).Velocity + (game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).Velocity * 0.35))
+        repeat task.wait() until game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).Velocity.X > 0
+        local newVelocity = (game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).Velocity + (game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).Velocity * 0.5))
         task.spawn(function()
             repeat
                 game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).Velocity = newVelocity
