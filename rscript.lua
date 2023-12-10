f3b7f8e3e0ace006dc20651b7e1605ad240de3b1
JustAsScripted <107677413+JustAsScripted@users.noreply.github.com>
2023-12-10 22:28:27 +0500

    Update rscript.lua

--- rscript.lua
+++ rscript.lua
@@ -1,16 +1,3 @@
-game.Players.LocalPlayer.Character.HumanoidRootPart.ChildAdded:Connect(function(nChild)
-    if nChild.ClassName == "BodyVelocity" then
-        repeat
-            game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).P = 2200
-            if game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).Velocity.Magnitude > 40 then
-                local newVelocity = (game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).Velocity + (game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).Velocity * 0.5))
-                repeat
-                    game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).P = 2200
-                    game.Players.LocalPlayer.Character.HumanoidRootPart:WaitForChild(nChild.Name).Velocity = newVelocity
-                    task.wait()
-                until not game.Players.LocalPlayer.Character.HumanoidRootPart:FindFirstChild(nChild.Name)
-            end
-            task.wait()
-        until not game.Players.LocalPlayer.Character.HumanoidRootPart:FindFirstChild(nChild.Name)
-    end
-end)
+for _,anim in pairs(game.Players.Castoutis.Character.Humanoid:GetPlayingAnimationTracks()) do
+    print(anim)
+end
